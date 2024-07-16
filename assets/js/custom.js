3b709a84997d6d4590bade3347be0bbff2ab88a9
Fadel Agli <aglifadel1@gmail.com>
2024-07-16 22:35:28 +0000

    La Vitrine V5

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+
+
 (function ($) {
 	
 	"use strict";
@@ -15,7 +17,7 @@
             var wow = new WOW ({
                 boxClass:     'wow',      // Animated element css class (default is wow)
                 animateClass: 'animated', // Animation css class (default is animated)
-                offset:       20,         // Distance to the element when triggering the animation (default is 0)
+                offset:       50,         // Distance to the element when triggering the animation (default is 0)
                 mobile:       true,       // Trigger animations on mobile devices (default is true)
                 live:         true,       // Act on asynchronously loaded content (default is true)
             });
